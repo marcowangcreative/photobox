75db07cb7a583d70178d5a05be368971eac7a5ae
marcowang-ai <marcowang@users.noreply.github.com>
2026-04-16 18:30:01 -0500

    Fix landscape rotation applying to desktop browsers at narrow widths
    Add pointer: coarse to the landscape media query so the 90-degree
    rotation only fires on touch devices, not desktop browsers resized
    below 1024px in landscape orientation.

--- components/PhotoGallery.tsx
+++ components/PhotoGallery.tsx
@@ -701,7 +701,7 @@ export default function PhotoGallery({ coupleNames, sneakPeekLabel, photos: rawP
 const baseStyles = `
   @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400&family=DM+Sans:wght@300;400;500&display=swap');
   * { box-sizing: border-box; margin: 0; padding: 0; outline: none; }
-  @media screen and (max-width: 1024px) and (orientation: landscape) {
+  @media screen and (max-width: 1024px) and (orientation: landscape) and (pointer: coarse) {
     html { transform: rotate(-90deg); transform-origin: top left;
       width: 100vh; height: 100vw; overflow-x: hidden;
       position: absolute; top: 100%; left: 0; }
